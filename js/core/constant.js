c19f745ae7d3654fab5dd405069f3b25fbc8f873
guilleriveraa <guilleriveraa12@gmail.com>
2026-03-13 11:27:45 +0100

    estoy hasta la polla

--- js/core/constant.js
+++ js/core/constant.js
@@ -2,9 +2,10 @@
 // Constantes globales de la aplicación
 
 // API
-window.API_URL = window.location.hostname === 'localhost' 
-    ? 'http://localhost:3000/api' 
-    : 'https://json-production-48ce.up.railway.app/api';
+window.API_URL = 'https://json-production-48ce.up.railway.app/api';
+// window.API_URL = window.location.hostname === 'localhost' 
+//     ? 'http://localhost:3000/api' 
+//     : 'https://json-production-48ce.up.railway.app/api';
 // LocalStorage keys
 window.TOKEN_KEY = 'svl_token';
 window.USER_KEY = 'svl_user';
